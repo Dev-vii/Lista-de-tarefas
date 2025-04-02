60b7d6b1b71f6dbdc32826de3bd290ad529279ab
Vicente <carvalho.vicente0207@gmail.com>
2025-04-02 15:27:23 -0300

    Add anotações de estudo

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const textBox = document.getElementById('TextBox');
 function Add() {
     const taskText = textBox.value.trim();
 
+    //Mensagem de erro
     if (taskText === "") {
         window.alert("Digite uma tarefa!");
         return;
     }
 
     //Criar elementos
-    const taskDiv = document.createElement('div');
+    const taskDiv = document.createElement('div'); //Cria uma div com uma classe. dentro dela, um parágrafo e o botão de excluir
     taskDiv.className = 'task';
 
     const taskP = document.createElement('p');
@@ -19,8 +20,9 @@ function Add() {
     const trash = document.createElement('img');
     trash.src = 'img/trash.png';
     trash.alt = 'Deletar';
-    trash.onclick = () => taskContainer.removeChild(taskDiv);
+    trash.onclick = () => taskContainer.removeChild(taskDiv); //Função para remover uma tarefa
 
+    //Mostar tudo na tela
     taskDiv.appendChild(taskP);
     taskDiv.appendChild(trash);
     taskContainer.appendChild(taskDiv);
